1062c9a8931f86e302744bb2e8d8475f629d522d
bmericc <bmericc@7ec27469-6037-0410-910e-9dbcb89bec78>
2007-12-04 19:50:08 +0000

    Copyright notice and license (GPLv3) notice in source code was added.

--- trunk/config.inc.php
+++ trunk/config.inc.php
@@ -1,11 +1,24 @@
 <?
 /*
+
  +-----------------------------------------------------------------------+
  | Domain Hunter - A Simple Domain Monitoring Application                |
  | Version 0.1.0                                                         |
  |                                                                       |
  | Copyright (C) 2006-2007, DomainLabs.EU - Turkey                       |
- | Licensed under the GNU GPLv3                                          |
+ |									 | 
+ | This program is free software: you can redistribute it and/or modify  |
+ | it under the terms of the GNU General Public License as published by  |
+ | the Free Software Foundation, either version 3 of the License, or     |
+ | (at your option) any later version.					 |
+ |									 |
+ | This program is distributed in the hope that it will be useful,	 |
+ | but WITHOUT ANY WARRANTY; without even the implied warranty of	 |
+ | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the	 |
+ | GNU General Public License for more details.				 |
+ |									 |
+ | You should have received a copy of the GNU General Public License	 |
+ | along with this program.  If not, see <http://www.gnu.org/licenses/>. |                                     |
  |                                                                       |
  +-----------------------------------------------------------------------+
  | Author: Bahri Meric CANLI <bahri@bahri.info>                          |
@@ -25,15 +38,14 @@ $hunter_email = "hostmaster@canlisoft.com";
 3- aboutuorg
 */
 
-$detail_server = 2;
+$detail_server = 1;
 
 ////////////////////////////////////////////////////////////////////
 
-$dbhost = "localhost";
-$dbuname = "hunter_user";
-$dbpass = "fVaQWW";
-$dbname = "domain_hunter";
-
+$dbhost   = "hostname"; // database hostname
+$dbuname  = "database_user";  // database user name
+$dbpass   = "password";  // database user password
+$dbname   = "database_name";  // database name
 
 //////////////////// mysql connect //////////////////////////////////
 
