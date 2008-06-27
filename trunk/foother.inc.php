f3c6edf192bbaec40d7116aa64ac74ff288bcfb8
bmericc <bmericc@7ec27469-6037-0410-910e-9dbcb89bec78>
2008-06-27 08:58:50 +0000

    Domainhunter was change project home page

--- trunk/foother.inc.php
+++ trunk/foother.inc.php
@@ -28,9 +28,9 @@
 
 ?>
 <div id="footer">
-<a target="_blank" href="http://www.domainhunter.gen.tr/">Domain Hunter <?php echo $script_version; ?></a>
+<a target="_blank" href="http://www.domainhunter.org.tr/">Domain Hunter <?php echo $script_version; ?></a>
 &nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp
-<a target="_blank" href="http://www.domainhunter.gen.tr/?version=<?php echo $script_version; ?>">Check for update</a>
+<a target="_blank" href="http://www.domainhunter.org.tr/?version=<?php echo $script_version; ?>">Check for update</a>
 
 </div>
 </body>
